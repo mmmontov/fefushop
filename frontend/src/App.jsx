5ef43984ba65fe77036410b15b0964e508d21ce7
Mikhail Mamontov <mmontovmm@gmail.com>
2025-11-09 01:21:31 +1000

    create user profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
